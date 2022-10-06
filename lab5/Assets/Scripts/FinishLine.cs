06f7e50c2f54ae23d37e589eb9cfe75096ded3ee
Nanthikan219-6EN842005 <87509180+Nanthikan@users.noreply.github.com>
2022-10-06 23:53:54 +0700

    lab5

---
+++ lab5/Assets/Scripts/FinishLine.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class FinishLine : MonoBehaviour
+{
+    [SerializeField] private PlayerAudioController audioController;
+    private const string PlayerTag = "Player";
+
+    private GameManager _gameManager;
+
+    private bool _triggeredNextScene;
+    [SerializeField] private AudioSource audiosource;
+    
+    // Why are we checking if the player reaches the finish line here? So, we do not
+    // have to check for every time the player collides with something for a finish line.
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (!col.CompareTag(PlayerTag)) return;
+
+        if (_triggeredNextScene) return;
+
+        _triggeredNextScene = true;   // This is to prevent double scene loads.
+        _gameManager = FindObjectOfType<GameManager>();
+        _gameManager.LoadNextLevel();
+        audioController.PlayWinningSound();
+        audiosource.Play();
+    }
+}
